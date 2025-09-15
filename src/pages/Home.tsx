fa7c92d9299bd8b8dac40182d44211d4aecef22a
kiranlal2 <kiranlal6dx@gmail.com>
2025-09-15 19:20:47 +0530

    automation setup added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 
 export default function Home() {
   return (
-    <div className="flex gap-5 border p-4 h-50">
-      <div className="p-2 bg-red-400 text-white">1box</div>
-      <div className="p-2 bg-red-500 text-white">2box</div>
-      <div className="p-2 bg-red-600 text-white">3box</div>
-      <div className="p-2 bg-red-700 text-white">4box</div>
-      <div className="p-2 bg-red-800 text-white">5box</div>
+    <div className="gap-5">
+      {/* sm(640px), md(768px), lg(1024px), xl(1280px), 2xl(1536) */}
+      <div className="main-container mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5">
+        <div className="box bg-red-50 text-red-500 p-5">1Box</div>
+        <div className="box bg-red-100 text-red-500 p-5" >2Box</div>
+        <div className="box bg-red-200 text-red-500 p-5" >3Box</div>
+        <div className="box bg-red-300 text-red-500 p-5" >4Box</div>
+        <div className="box bg-red-400 p-5" >5Box</div>
+        <div className="box bg-red-500 p-5" >6Box</div>
+        <div className="box bg-red-600 p-5" >7Box</div>
+        <div className="box bg-red-700 p-5" >8Box</div>
+        <div className="box bg-red-800 p-5" >9Box</div>
+      </div>
     </div>
+    
   )
 }
